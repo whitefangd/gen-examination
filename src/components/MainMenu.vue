82e49b617d65b0cf5e751d45e4785ed3f517967a
whitefangd <boluclac.mamnem@gmail.com>
2020-02-23 13:50:22 +0700

    Dang phat trien

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -2,15 +2,18 @@
   <v-navigation-drawer v-model="drawer" app clipped>
     <v-list dense>
       <template v-for="menu in menus">
-        <v-list-group :key="menu.id" v-if="menu.isGroup">
+        <v-list-group :key="menu.id" v-if="menu.isGroup" no-action>
           <template v-slot:activator>
+            <v-list-item-icon>
+              <v-icon>{{ menu.icon }}</v-icon>
+            </v-list-item-icon>
             <v-list-item-title>{{ $t(menu.title)}}</v-list-item-title>
           </template>
-          <v-list-item :key="subItem.id" v-for="subItem in menu.children" link >
-            <v-list-item-icon>
-              <v-icon>{{ subItem.icon }}</v-icon>
-            </v-list-item-icon>
+          <v-list-item :key="subItem.id" v-for="subItem in menu.children" link>
             <v-list-item-title>{{ $t(subItem.title)}}</v-list-item-title>
+            <v-list-item-icon>
+              <v-icon>{{ menu.icon }}</v-icon>
+            </v-list-item-icon>
           </v-list-item>
         </v-list-group>
         <v-list-item :key="menu.id" v-else link>
